649aa4ad26758f1d6b8731314fb56af73b8ad5a5
Marco Trombino <trombino.marco@gmail.com>
2016-12-17 22:00:02 +0100

    Release v1.0.1: Added scope value. Fixed scope value type on getter

--- dist/bridge.js
+++ dist/bridge.js
@@ -1,5 +1,5 @@
 /** Bridge lightweight library for two-way data binding
- * @version: 1.0.0
+ * @version: 1.0.1
  * @date: 2016-12-15
  * UPDATE AND DOCS AT: https://github.com/Marcotrombino/Bridge
  * @copyright (C) 2016 Marco Trombino
@@ -15,108 +15,124 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses.
  */
+  if(typeof Bridge == 'undefined' || !Bridge) {
+    var Bridge = (function() {
+      // Bridge reference
+        var $ = {};
+      // Bridge scope
+        $.scope = {};
 
-  var Bridge = (function() {
-    // Bridge reference
-      var $ = {};
-    // Bridge scope
-      $.scope = {};
-    // Parsing methods
-      var parseTo = {
-        number: function(data) {return Number(data);},
-        int: function(data) {return parseInt(data);},
-        float: function(data) {return parseFloat(data);},
-        string: function(data) {return String(data);}
-      };
+        /****************** UTILITIES ******************/
 
-    // Implements two-way binding
-      var binder = function() {
-        var DOM = document.querySelectorAll("[data-bind]");
-        DOM.forEach(function(el) {
-          var bindVar = el.dataset.bind;
-          if(!$.hasOwnProperty(bindVar)) {
-            $.scope[bindVar] = {
-              type: undefined,
-              nodes: []
-            };
-            Object.defineProperty($, bindVar, {
-              get: function() {
-                return binderGetter(el);
-              },
-              set: function(newVal) {
-                $.scope[bindVar].type = typeof newVal;
-                var nodes = $.scope[bindVar].nodes;
-                var iterations = nodes.length;
-                for(var i=0; i<iterations; ++i)
-                  binderSetter(nodes[i], newVal)
-              }
-            });
+        // Parsing methods
+        var parseTo = {
+          number: function(data) {return Number(data);},
+          int: function(data) {return parseInt(data);},
+          float: function(data) {return parseFloat(data);},
+          string: function(data) {return String(data);}
+        };
+
+        /***********************************************/
+
+        // data binding on particular DOM elements events
+        var DOMEvents = {
+          input: {key: "value", events: ["input", "onkeydown"]},
+          textarea: {key: "value", events: ["input", "onkeydown"]}
+        };
+
+        /**
+        *  Sync DOM element value with model based on HTMLElement
+        *  @method DOMSetter
+        *  @param {HTMLElement} el : DOM element with "data-bind" attribute
+        *  @param {String} newVal : New scope variable value
+        */
+        var DOMSetter = function(el, newVal) {
+          var scopeVar = el.dataset.bind;
+          switch(el.nodeName.toLowerCase()) {
+            case "input":
+            case "textarea":
+              el.value = newVal;
+              break;
+            case "img":
+              el.src = newVal;
+            default:
+              el.innerHTML = newVal;
           }
+        };
 
-          // Push node into scope
-          $.scope[bindVar].nodes.push(el);
+        // Bridge watchers: "data-" attribute to watch
+          var watcher = {};
 
-          // Binder Events
-          if(binderEvents.hasOwnProperty(el.nodeName.toLowerCase())) {
-            var tagName = binderEvents[el.nodeName.toLowerCase()];
-            var events = tagName.events;
-            var key = tagName.key;
-            for(var i=0; i<events.length; ++i) {
-              el[events[i]] = function() {
-                var nodes = $.scope[bindVar].nodes;
-                var iterations = nodes.length;
-                for(var i=0; i<iterations; ++i)
-                  binderSetter(nodes[i], el[key])
+          /**
+          *  Sync DOM and Model through "data-bind" attribute
+          *  @method watcher.bind
+          *  @param {HTMLElement} el : DOM element with "data-bind" attribute
+          *  @param {String} bindVar : Scope variable name
+          */
+          watcher.bind = function(el, scopeVar) {
+            // if scope variable is not defined
+            if(!$.hasOwnProperty(scopeVar)) {
+              // define scope variable
+              $.scope[scopeVar] = {
+                watcher: "bind",       // type of watcher
+                value: undefined,      // variable value
+                nodes: []              // binded DOM nodes
               };
+
+              // create scope variable API
+              Object.defineProperty($, scopeVar, {
+                // define scope variable getter
+                get: function() {
+                  return $.scope[scopeVar].value;        // GET scope variable value
+                },
+
+                // define scope variable setter
+                set: function(newVal) {
+                  $.scope[scopeVar].value = newVal;      // SET scope variable value
+                  /* --- SET DOM nodes ---*/
+                  var nodes = $.scope[scopeVar].nodes;
+                  var iterations = nodes.length;
+                  for(var i=0; i<iterations; ++i)
+                    DOMSetter(nodes[i], newVal);
+                }
+              });
+            } // scope variable is not defined
+
+            // Push DOM element into scope
+            $.scope[scopeVar].nodes.push(el);
+
+            // Sync DOM and Model on DOM element's events
+            if(DOMEvents.hasOwnProperty(el.nodeName.toLowerCase())) {
+              var tagName = DOMEvents[el.nodeName.toLowerCase()];
+              var events = tagName.events;
+              var key = tagName.key;
+              for(var i=0; i<events.length; ++i) {
+                el.addEventListener(events[i], function() {
+                  $.scope[scopeVar].value = el[key];
+                  var nodes = $.scope[scopeVar].nodes;
+                  var iterations = nodes.length;
+                  for(var i=0; i<iterations; ++i)
+                    DOMSetter(nodes[i], el[key]);
+                });
+              }
             }
-          }
+          };
 
-        });
-      };
+      // Implements two-way binding
+        var binder = function(watcher) {
+          Object.keys(watcher).forEach(function (watchAttr) {
+            var DOM = document.querySelectorAll("[data-" + watchAttr + "]");
+            DOM.forEach(function(el) {
+              var scopeVar = el.dataset[watchAttr];
+              // Call watcher method
+              watcher[watchAttr](el, scopeVar);
+            });
+          });
+        };
 
-      // Events to attach on HTML events
-      var binderEvents = {
-        input: {
-          key: "value",   // HTML value to change
-          events: ["oninput", "onkeydown"]  // events to attach
-        },
-        textarea: {
-          key: "value",
-          events: ["oninput", "onkeydown"]  // events to attach
-        }
-      };
-
-      // Get bind data from HTML elements
-      var binderGetter = function(el) {
-        var bindVar = el.dataset.bind;
-        switch(el.nodeName.toLowerCase()) {
-          case "input":
-          case "textarea":
-            return parseTo[$.scope[bindVar].type](el.value);
-            break;
-          case "img":
-            return el.src;
-            break;
-          default:
-            return parseTo[$.scope[bindVar].type](el.innerHTML);
-        }
-      };
-
-      // Set bind data into HTML elements
-      var binderSetter = function(el, newVal) {
-        var bindVar = el.dataset.bind;
-        switch(el.nodeName.toLowerCase()) {
-          case "input":
-          case "textarea":
-            el.value = newVal;
-            break;
-          case "img":
-            el.src = newVal;
-          default:
-            el.innerHTML = newVal;
-        }
-      };
-
-    binder();
-    return $;
-  }());
+      binder(watcher);
+      return $;
+    }());
+    // check "$" namespace availability
+    if(typeof $ == 'undefined') var $ = Bridge;
+  } else console.error("'Bridge' namespace's not available.");
